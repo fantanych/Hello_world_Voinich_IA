58f011db3bc06de2a02fdc49cb688784ffa7f855
fantanych <yarisunok@gmail.com>
2026-03-05 11:03:14 +0300

    Delete projects_2/task_2-2_3.py

--- projects_2/task_2-2_3.py
+++
@@ -1,6 +0,0 @@
-device_name = "Микроскоп"
-inventory_number = "INV-2024-001"
-is_working = "исправен"
-quantity = 3
-print("Название прибора\tИнвентарный номер\tСостояние\tКоличество")
-print(f"{device_name}\t\t{inventory_number}\t\t{is_working}\t\t{quantity}")
