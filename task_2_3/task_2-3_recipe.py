b5862a2b526e98b230dc47944ac447c874226deb
fantanych <yarisunok@gmail.com>
2026-03-05 11:00:53 +0300

    Delete task_2_3 directory

--- task_2_3/task_2-3_recipe.py
+++
@@ -1,8 +0,0 @@
-sreda_name = input ("Введите название питательной среды: ")
-konc_agar = input ("Введите концентрацию агара (%): ")
-temp_sterilize = input ("Введите температуру стерилизации (°C): ")
-report = f"Питательная среда: {sreda_name}\nКонцентрация агара (%): {konc_agar}\nТемпература стерилизации (°C): {temp_sterilize}"
-print(report)
-with open("recipe.txt", "w", encoding="utf-8") as file:
-    file.write(report)
-    print ('Файл "recipe.txt" успешно сформирован!')
--- task_2_3/task_2-3_sensor.py
+++
@@ -1,7 +0,0 @@
-operator_name = input("Введите имя оператора: ")
-pressure_value = input("Введите текущее значение давления (Па): ")
-log_entry = f"{operator_name}\t\t{pressure_value}"
-with open("sensor_log.txt", "w", encoding="utf-8") as file:
-    file.write("ОПЕРАТОР\tЗНАЧЕНИЕ\n")
-    file.write(log_entry)
-print("Данные успешно сохранены в sensor_log.txt")  
